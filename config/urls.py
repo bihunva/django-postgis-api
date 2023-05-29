b5054feed3be3e39ce64a005bf794ad172ee6d0a
Vadym Bihun <bihunva@gmail.com>
2023-05-29 08:53:00 +0300

    Add api prefix to endpoint paths of place app

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("places/", include("places.urls")),
+    path("api/places/", include("places.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/doc/swagger/",
