f9bd2ef6cba36c14e5fc249419f85a74322ca9e3
Vadym Bihun <bihunva@gmail.com>
2023-05-29 08:18:52 +0300

    Create ViewSet for Place model

--- places/views.py
+++ places/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.contrib.gis.db.models.functions import GeometryDistance
+from django.contrib.gis.geos import Point
+from rest_framework import viewsets
+from rest_framework.exceptions import ParseError
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Place
+from .serializers import PlaceSerializer
+
+SRID = 4326
+
+
+class PlaceViewSet(viewsets.ModelViewSet):
+    queryset = Place.objects.all()
+    serializer_class = PlaceSerializer
+
+    def list(self, request, *args, **kwargs) -> Response:
+        """
+        Get the nearest place to the given coordinates if provided,
+        otherwise return a list of places.
+        """
+        longitude = float(request.query_params.get("longitude", 0))
+        latitude = float(request.query_params.get("latitude", 0))
+
+        if longitude and latitude:
+            reference_point = Point(longitude, latitude, srid=SRID)
+            return self.find_nearest_place(reference_point)
+
+        if longitude or latitude:
+            raise ParseError("Please provide both longitude and latitude")
+
+        return super().list(request, *args, **kwargs)
+
+    def find_nearest_place(self, point: Point) -> Response:
+        """
+        Find the nearest place to the given reference point,
+        returning a corresponding message if no place is found.
+        """
+        places = self.queryset.annotate(distance=GeometryDistance("geom", point))
+        nearest_place = places.order_by("distance").first()
+
+        if not nearest_place:
+            return Response({"message": "No nearest place found."})
+
+        return Response(self.get_serializer(nearest_place).data)
